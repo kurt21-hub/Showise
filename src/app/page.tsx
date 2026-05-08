d13438fc4ba94e6edaa682d0fb334acaf80f8c20
kurt21 <daquigan.kurtangelo@dnsc.edu.ph>
2026-05-08 09:36:52 +0800

    fix build issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
+import Link from "next/link";
 import React from 'react';
-import Link from 'next/link';
 import { motion } from 'framer-motion';
 import { Scan, Send } from 'lucide-react';
 import { Button } from '@/components/ui/button';
